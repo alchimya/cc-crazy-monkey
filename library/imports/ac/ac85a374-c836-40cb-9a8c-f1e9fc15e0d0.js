eb3326c1197253e8d3188fa28bf9ba42ecfd7825
Domenico Vacchiano <info info@domenicovacchiano.com>
2016-04-30 15:55:17 +0100

    Added classes and components for UserDefault implementations with cc.sys.localStorage object

---
+++ library/imports/ac/ac85a374-c836-40cb-9a8c-f1e9fc15e0d0.js
@@ -0,0 +1,32 @@
+var UserDefaultKeys = require('user-default-keys')();
+var UserDefault = cc.Class({
+    'extends': cc.Component,
+
+    properties: {
+        localStorage: {
+            'default': null,
+            visible: false,
+            type: Object
+        }
+    },
+
+    // use this for initialization
+    onLoad: function onLoad() {
+        this.localStorage = cc.sys.localStorage;
+        //init the singleton instance
+        UserDefault.instance = this;
+    },
+    statics: {
+        instance: null
+    },
+    getCurrentCredit: function getCurrentCredit(defaultValue) {
+        var data = this.localStorage.getItem(UserDefaultKeys.CURRENT_CREDIT);
+        if (!data) {
+            data = defaultValue;
+        }
+        return data;
+    },
+    setCurrentCredit: function setCurrentCredit(value) {
+        this.setItem.getItem(UserDefaultKeys.CURRENT_CREDIT, value);
+    }
+});
---
+++ temp/scripts/assets/scripts/controllers/user-default.js
@@ -0,0 +1,38 @@
+"use strict";
+cc._RFpush(module, 'ac85aN0yDZAy5qM8en8FeDQ', 'user-default');
+// scripts/controllers/user-default.js
+
+var UserDefaultKeys = require('user-default-keys')();
+var UserDefault = cc.Class({
+    'extends': cc.Component,
+
+    properties: {
+        localStorage: {
+            'default': null,
+            visible: false,
+            type: Object
+        }
+    },
+
+    // use this for initialization
+    onLoad: function onLoad() {
+        this.localStorage = cc.sys.localStorage;
+        //init the singleton instance
+        UserDefault.instance = this;
+    },
+    statics: {
+        instance: null
+    },
+    getCurrentCredit: function getCurrentCredit(defaultValue) {
+        var data = this.localStorage.getItem(UserDefaultKeys.CURRENT_CREDIT);
+        if (!data) {
+            data = defaultValue;
+        }
+        return data;
+    },
+    setCurrentCredit: function setCurrentCredit(value) {
+        this.setItem.getItem(UserDefaultKeys.CURRENT_CREDIT, value);
+    }
+});
+
+cc._RFpop();
